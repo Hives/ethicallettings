0bc22cd36a08f4c02f0bf4401a2267068d29e7aa
Hives <communty.hivemind@gmail.com>
2018-09-28 16:07:43 +0100

    Disable flexbox homepage boxes in IE

--- functions.php
+++ functions.php
@@ -387,3 +387,33 @@ function el_location_map_shortcode( $atts, $content = null ) {
 	return '<img src="' . get_stylesheet_directory_uri() . '/images/ethical-lettings-map.png" alt="Mapp">';
 }
 add_shortcode( 'location_map', 'el_location_map_shortcode' );
+
+/**
+ * Add browser class to body element.
+ *
+ * @link https://www.wpbeginner.com/wp-themes/how-to-add-user-browser-and-os-classes-in-wordpress-body-class/
+ */
+function mv_browser_body_class($classes) {
+        global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+        if($is_lynx) $classes[] = 'lynx';
+        elseif($is_gecko) $classes[] = 'gecko';
+        elseif($is_opera) $classes[] = 'opera';
+        elseif($is_NS4) $classes[] = 'ns4';
+        elseif($is_safari) $classes[] = 'safari';
+        elseif($is_chrome) $classes[] = 'chrome';
+        elseif($is_IE) {
+                $classes[] = 'ie';
+                if(preg_match('/MSIE ([0-9]+)([a-zA-Z0-9.]+)/', $_SERVER['HTTP_USER_AGENT'], $browser_version))
+                $classes[] = 'ie'.$browser_version[1];
+        } else $classes[] = 'unknown';
+        // if($is_iphone) $classes[] = 'iphone';
+        // if ( stristr( $_SERVER['HTTP_USER_AGENT'],"mac") ) {
+        //          $classes[] = 'osx';
+        //    } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"linux") ) {
+        //          $classes[] = 'linux';
+        //    } elseif ( stristr( $_SERVER['HTTP_USER_AGENT'],"windows") ) {
+        //          $classes[] = 'windows';
+        //    }
+        return $classes;
+}
+add_filter('body_class','mv_browser_body_class');
